7b3e1802027c311b7cb161d5588116eff4800861
Jonatan Kr <jonatan.kronander@gmail.com>
2019-10-10 18:13:21 +0200

    task 1 and 2 done

--- handin_3/task1_2/main_svm.jl
+++ handin_3/task1_2/main_svm.jl
@@ -51,6 +51,7 @@ function svm(x, y, σ, λ, itrs, gradient)
 	w  = ones(size(Q)[1],itrs)
 	w_k_half = ones(size(Q)[1])
 	t_k0 = 1
+	μ = 5000 # tuned until convergence minimum eigenvalue?
 
 	for i = 3:itrs
 		if gradient == 0
@@ -64,8 +65,7 @@ function svm(x, y, σ, λ, itrs, gradient)
 			w_k_half = w[:,i-1] + β * (w[:,i-1] - w[:,i-2])
 			t_k0 = t_k1
 		elseif gradient == 3
-			μ = 100
-			β = (1-sqrt(μ * λ)) / (1+sqrt(μ * λ))
+			β = (1-sqrt(μ * step_size)) / (1+sqrt(μ * step_size))
 			w_k_half = w[:,i-1] + β * (w[:,i-1] - w[:,i-2])
 		end
 		grad_Q = Q*w_k_half
@@ -98,11 +98,60 @@ function model(w,x,y,x_pred,λ,σ)
 	return y_pred
 end
 
-############################
-# TesT
-############################
+function svm_cord(x, y, σ, λ, itrs, step_size_ii = 0)
+	"""
+	Trains model weights w
+	:param: x data
+	:param: y data
+	:param: sigma - lenght-scale, smoothness param
+	:param: lambda - Regularization factor
+	:param: iterations
+	:return: model weight vector
+	"""
 
-function test_1(test)
+	N = 1/length(x)
+	y_i = ones(1)
+	hx = HingeLoss(y_i,N)
+	h_con = Conjugate(hx)
+
+	Y = Diagonal(y)
+
+	K = Kernel(x,σ)
+
+	Q = inv(λ)*(Y*K*Y) # 1/lambda*2 v'Qv = 1/2 v'YX'XYv from manual
+	step_size = inv(opnorm(Q))
+
+	## Implement coordinate decent
+
+	w_n  = ones(size(Q)[1],1)
+	w_itrs = Int(floor(itrs/1000))
+	w  = ones(size(Q)[1],w_itrs)
+	i_w = 1
+	for i = 1:itrs-1
+		sample_i = rand(1:length(x)) #Uniform choise
+
+		if i % 1000 == 0 # For plot
+			w[:,i_w] = w_n
+			i_w = i_w + 1
+		end
+		if step_size_ii == 1 # coordinate wise step-size
+			step_size = 1/Q[sample_i,sample_i]
+		end
+
+		grad_Q = Q[sample_i,:]'*w_n
+		wk, _ = prox(h_con, [w_n[sample_i]-step_size *  grad_Q[1]], step_size) # Why [] in proxy? See manual task 2...
+		w_n[sample_i] = wk[1]
+	end
+
+	return w
+end
+
+############################
+############################
+############################
+###### TASK 1
+function task_1(test)
+	print("---------- TASK 1 ----------\n")
 	x,y = svm_train()
 	λ = 0.0001
 	σ = 0.25
@@ -195,7 +244,7 @@ function test_1(test)
 		itrs_w3[i] = opnorm((w3[:,i]-w0[:,end])')
 	end
 
-	ploty = 300
+	ploty = 250
 	plot(log.(itrs_w0), xlims=[0,ploty], label = "w0", margin=5Plots.mm)
 	ylabel!("log(||w_i-w^*||)")
 	xlabel!("iterations")
@@ -203,7 +252,60 @@ function test_1(test)
 	plot!(log.(itrs_w2), xlims=[0,ploty], label = "w2", margin=5Plots.mm,  linestyle = :dash)
 	plot!(log.(itrs_w3), xlims=[0,ploty], label = "w3", margin=5Plots.mm)
 
-	savefig("/Users/JonatanMBA/google drive/lth/frtn50/handin_3/task1/plots/1.png")
+	savefig("/Users/JonatanMBA/google drive/lth/frtn50/handin_3/task1_2/plots/1.png")
 end
 
-test_1(1)
+task_1(1)
+
+############################
+############################
+############################
+###### TASK 2
+function task_2()
+	print("---------- TASK 2 ----------\n")
+	x,y = svm_train()
+	λ = 0.0001 # from handin 2
+	σ = 0.25 # from handin 2
+
+	print("Training model, w0... \n")
+	@time w0 = svm(x,y,σ,λ,110,0)
+	print("Training model, w1... \n")
+	@time w1 = svm(x,y,σ,λ,200,1)
+	print("Training model, w2... \n")
+	@time w2 = svm(x,y,σ,λ,200,2)
+	print("Training model, w3... \n")
+	@time w3 = svm(x,y,σ,λ,60,3)
+	print("Training model, coord uniform choise step size... \n")
+	@time w4 = svm_cord(x,y,σ,λ,65000,0)
+	print("Training model, coord coordinate wise step size... \n")
+	@time w5 = svm_cord(x,y,σ,λ,35000,1)
+
+
+	itrs_w0 = ones(size(w0)[2])
+	itrs_w4 = ones(size(w4)[2])
+	itrs_w5 = ones(size(w5)[2])
+	x_coord_plot4 = ones(size(w4)[2])
+	x_coord_plot5 = ones(size(w5)[2])
+	for i = 1:length(itrs_w0)-1
+		itrs_w0[i] = opnorm((w0[:,i]-w0[:,end])')
+	end
+	for i = 1:length(itrs_w4)-1
+		itrs_w4[i] = opnorm((w4[:,i]-w0[:,end])')
+		x_coord_plot4[i] = 1000 * (i-1)
+	end
+	for i = 1:length(itrs_w5)-2
+		itrs_w5[i] = opnorm((w5[:,i]-w0[:,end])')
+		x_coord_plot5[i] = 1000 * (i-1)
+	end
+
+	ploty = 65000
+	plot(log.(itrs_w0), xlims=[0,ploty], label = "w0", margin=5Plots.mm)
+	ylabel!("log(||w_i-w^*||)")
+	xlabel!("iterations")
+	plot!(x_coord_plot4[1:end-2], log.(itrs_w4)[1:end-2], xlims=[0,ploty], label = "uniform choise step size", margin=5Plots.mm)
+	plot!(x_coord_plot5[1:end-2], log.(itrs_w5)[1:end-2], xlims=[0,ploty], label = "coordinate wise step size", margin=5Plots.mm)
+
+	savefig("/Users/JonatanMBA/google drive/lth/frtn50/handin_3/task1_2/plots/2.png")
+end
+
+task_2()
